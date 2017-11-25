1a8422d8eb43d53a0c1e1e499844a69185930278
Abi Travers <atravers4007@gmail.com>
2017-11-25 12:17:56 +0000

    First test. Checking word counter module is initialised with text

--- test/word-count-test.js
+++ test/word-count-test.js
@@ -1,10 +1,23 @@
 var expect  = require('chai').expect;
 var assert = require('chai').assert;
+const WordCounter = require('../app/wordCounter').WordCounter;
 
-describe('Array', function() {
-  it('should start empty', function() {
-    var arr = [];
+describe('Word Counter', () => {
+  var wordCounter;
 
-    assert.equal(arr.length, 0);
+  beforeEach( () => {
+    wordCounter = new WordCounter("Test text");
+  })
+
+  describe('Initialize',() => {
+    it("A new instance is initialised with text as an argument", () => {
+      expect(wordCounter.initialText).to.equal("Test text")
+    });
   });
 });
+
+// it('should start empty', function() {
+//   var arr = [];
+//
+//   assert.equal(arr.length, 0);
+// });

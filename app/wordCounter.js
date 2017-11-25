ea70cd9e8acef24be2c280a3b93e33ceacb39950
Abi Travers <atravers4007@gmail.com>
2017-11-25 17:24:35 +0000

    Adding tests to check validity of input text and show an error message if it is not a string

--- app/wordCounter.js
+++ app/wordCounter.js
@@ -2,6 +2,13 @@ function WordCounter(providedText) {
   this.initialText = providedText;
 };
 
+WordCounter.prototype.checkInputValidator= function (){
+  if (this.initialText === !String) {
+    console.log("I am sorry but I cannot count the words of something which isn't text Please enter text");
+    this.initialText = false;
+  }
+}
+
 WordCounter.prototype.RemovePunctuation = function() {
   this.initialText = this.initialText.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
 }
--- test/word-count-test.js
+++ test/word-count-test.js
@@ -1,6 +1,9 @@
-var expect  = require('chai').expect;
-var assert = require('chai').assert;
+const expect  = require('chai').expect;
+const assert = require('chai').assert;
+const sinon  = require('sinon');
 const WordCounter = require('../app/wordCounter').WordCounter;
+
+
 
 describe('Word Counter', () => {
   var wordCounter;
@@ -11,12 +14,41 @@ describe('Word Counter', () => {
       wordCounter = new WordCounter("Test text");
     });
 
+    // it("Is set up as a constructor function correctly so a new instance of it can be created.", () => {
+    //   expect(wordCounter).to.be.a.wordCounter;
+    // });
+
     it("A new instance is initialised with text as an argument", () => {
       expect(wordCounter.initialText).to.equal("Test text");
-      expect(wordCounter.initialText).to.be.a('string');
+      expect(wordCounter.initialText).to.have.lengthOf(9);
     });
 
   });
+
+
+
+  describe('CheckInputValidator',() => {
+
+    it("Outputs an error message if word Counter is initalised with something which is not a string", () => {
+      wordCounter = new WordCounter(0000099090);
+      let spy = sinon.spy(console, 'log');
+
+      wordCounter.checkInputValidator();
+      expect(spy.calledWith("I am sorry but I cannot count the words of something which isn't text Please enter text"))
+      expect(wordCounter.initialText).to.equal(false);
+
+      spy.restore();
+    });
+
+    it("Continues with initalText set as intended in initalised if it is passed a string", () => {
+      wordCounter = new WordCounter("Test text");
+
+      wordCounter.checkInputValidator();
+      expect(wordCounter.initialText).to.be.a('string');
+    });
+  });
+
+
 
   describe('RemovePunctuation',() => {
 
@@ -28,6 +60,7 @@ describe('Word Counter', () => {
     });
 
     it("It removes all punctuation from a larger text file", () => {
+
       const inputText = "Contents. I.    The beginning of things. II.   Peter's coal-mine.III.  The old gentlem XIII. The hound's grandfatherXIV.  The End.Chapter I. The beginning of things."
       const expectedOutput = "Contents I    The beginning of things II   Peter's coalmineIII  The old gentlem XIII The hound's grandfatherXIV  The EndChapter I The beginning of things"
 

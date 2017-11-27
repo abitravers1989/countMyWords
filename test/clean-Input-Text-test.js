8d0b10eb88421111c9e1310bd99ff96ebf7e546f
Abi Travers <atravers4007@gmail.com>
2017-11-27 14:38:32 +0000

    Refactoring test for better readability

--- test/clean-Input-Text-test.js
+++ test/clean-Input-Text-test.js
@@ -5,6 +5,21 @@ const CleanInputText = require('../app/cleanInputText').CleanInputText;
 describe('CleanInputText', () => {
   var cleanInputText;
 
+
+  const testInputText1 = "Test text";
+  const testInputNumber2 = 0000099090;
+  const testInputText3 = "Test text!!!???? ?I.i'ds':$$ *HI but---";
+  const expectedOutput3 = "Test text Iids HI but";
+  const testInputText4 = "!?'/.:$*HI-+)(&{/}^%£@|±~``§,;=-end";
+  const expectedOutput4 = "HIend";
+  const testInputText5 = "Contents. I.    The beginning of things. II.   Peter's coal-mine.III.  The old gentlem XIII. The hound's grandfatherXIV.  The End.Chapter I. The beginning of things."
+  const expectedOutput5 = "Contents I The beginning of things II Peters coalmineIII The old gentlem XIII The hounds grandfatherXIV The EndChapter I The beginning of things"
+  const testInputText7 = "Test TeXT";
+  const expectedOutput7 = "test text";
+  const testInputText8 = "Test TeXT LonGer Edition!!!!";
+  const expectedOutput8 = "test text longer edition";
+
+
   beforeEach(() => {
     cleanInputText = new CleanInputText();
   });
@@ -22,7 +37,6 @@ describe('CleanInputText', () => {
   describe('checkInputValidator',() => {
 
     it("Sets initalText to the text supplied to it, if this is indeed text and not something else such as a picture or number.", () => {
-      const testInputText1 = "Test text";
       cleanInputText.checkInputValidator(testInputText1);
       expect(cleanInputText.initialText).to.be.a('string');
       expect(cleanInputText.initialText).to.have.lengthOf(9);
@@ -30,7 +44,6 @@ describe('CleanInputText', () => {
 
 
     it("Outputs an error message if word Counter is initalised with something which is not a string", () => {
-      const testInputNumber2 = 0000099090;
       let spy = sinon.spy(console, 'log');
       cleanInputText.checkInputValidator(testInputNumber2);
       expect(spy.calledWith("I am sorry but I cannot count the words of something which isn't text Please enter text"))
@@ -43,14 +56,6 @@ describe('CleanInputText', () => {
 
 
   describe('removePunctuationAndWhitespace',() => {
-    const testInputText3 = "Test text!!!???? ?I.i'ds':$$ *HI but---";
-    const expectedOutput3 = "Test text Iids HI but";
-    const testInputText4 = "!?'/.:$*HI-+)(&{/}^%£@|±~``§,;=-end";
-    const expectedOutput4 = "HIend";
-    const testInputText5 = "Contents. I.    The beginning of things. II.   Peter's coal-mine.III.  The old gentlem XIII. The hound's grandfatherXIV.  The End.Chapter I. The beginning of things."
-    const expectedOutput5 = "Contents I The beginning of things II Peters coalmineIII The old gentlem XIII The hounds grandfatherXIV The EndChapter I The beginning of things"
-
-
 
     it("Removes punctuation from text", () => {
       cleanInputText.checkInputValidator(testInputText3);
@@ -76,11 +81,9 @@ describe('CleanInputText', () => {
   describe('removeCapitalisation',() => {
 
     it("It converts capital letters to downcase", () => {
-      const testInputText6 = "Test TeXT";
-      const expectedOutput6 = "test text";
-      cleanInputText.checkInputValidator(testInputText6);
+      cleanInputText.checkInputValidator(testInputText7);
       cleanInputText.removeCapitalisation();
-      expect(cleanInputText.initialText).to.equal(expectedOutput6)
+      expect(cleanInputText.initialText).to.equal(expectedOutput7)
     });
 
   });
@@ -89,10 +92,8 @@ describe('CleanInputText', () => {
   describe('cleanTextFunctions',() => {
 
     it("Calls all the above functions which clean the text so it contains no capitalisation or punctuation", () => {
-      const testInputText7 = "Test TeXT LonGer Edition!!!!";
-      const expectedOutput7 = "test text longer edition";
-      cleanInputText.cleanTextFunctions(testInputText7)
-      expect(cleanInputText.initialText).to.equal(expectedOutput7)
+      cleanInputText.cleanTextFunctions(testInputText8)
+      expect(cleanInputText.initialText).to.equal(expectedOutput8)
     });
 
   });
